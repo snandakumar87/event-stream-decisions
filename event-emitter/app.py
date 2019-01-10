345cf3e9941637371f58042b45f0953805e8079e
Sadhana Nandakumar <39506194+snandakumar87@users.noreply.github.com>
2019-01-10 07:54:41 -0500

    Update app.py

--- event-emitter/app.py
+++ event-emitter/app.py
@@ -53,7 +53,7 @@ def main(args):
     logging.info('begin sending events')
     while True:
         logging.info('here we are')
-        producer.send(args.topic, ('here we are').encode('UTF-8'))
+        producer.send(args.topic, json.dumps(generate_event()).encode())
         time.sleep(1.0 / int(args.rate))
     logging.info('end sending events')
 
